a52038b54fdace08ff756ad4cffceb7eddf7fd40
Tafita Hery <tafitahery@gmail.com>
2022-10-02 21:05:07 +0300

    Create Pure React Slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Announcement from '../components/Announcement';
 
 import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
 
 export default function Home() {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 }
