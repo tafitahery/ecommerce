7dd0f031519f4e4f8103b76a1693b98ba35bbb0e
Tafita Hery <tafitahery@gmail.com>
2022-10-03 20:11:40 +0300

    React Newsletter Design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
-
 import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
 import Products from '../components/Products';
 import Slider from '../components/Slider';
 
@@ -14,6 +13,7 @@ export default function Home() {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   );
 }
