9db3b55d0524e68d281a32f4c7bd0b66e095521a
Tafita Hery <tafitahery@gmail.com>
2022-10-04 20:46:52 +0300

    React Login and Register Page Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 // import Home from './pages/Home';
-import Product from './pages/Product';
+// import Product from './pages/Product';
+import Login from './pages/Login';
+// import Register from './pages/Register';
 // import ProductList from './pages/ProductList';
 
 const App = () => {
   return (
     // <Home />;
     // <ProductList />
-    <Product />
+    // <Product />
+    // <Register />
+    <Login />
   );
 };
 
