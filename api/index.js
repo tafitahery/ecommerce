09d30cb038bcfbe05ab6eb26213514fc6f1669b7
Tafita Hery <tafitahery@gmail.com>
2022-10-05 20:52:52 +0300

    Node.js MongoDB Connexion

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connexion to MongoDB successfully'))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running on port 5000');
 });
