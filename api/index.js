9d0ee20320dbd9b27f65eff90117160122512a70
Tafita Hery <tafitahery@gmail.com>
2022-10-05 21:41:15 +0300

    Node.js Routes and MongoDB Models

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user');
+
 const app = express();
 dotenv.config();
 
@@ -9,6 +11,10 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('Connexion to MongoDB successfully'))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running on port 5000');
