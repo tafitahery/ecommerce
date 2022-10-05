05c4973b860dd5332a8ddaacf6b09f38949739be
Tafita Hery <tafitahery@gmail.com>
2022-10-05 22:46:16 +0300

    Node.js MongoDB Authentification

---
+++ api/routes/auth.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const CryptoJS = require('crypto-js');
+
+const User = require('../models/User');
+
+//REGISTER
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    ...req.body,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json('Wrong credentials');
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (req.body.password !== hashedPassword) {
+      return res.status(401).json('Wrong credentials');
+    }
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
