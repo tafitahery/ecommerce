4c8b374e7d54c8da7e2018b925ab7ae90b07ab94
Tafita Hery <tafitahery@gmail.com>
2022-10-06 18:03:18 +0300

    Advanced MongoDB Functions

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 const app = express();
 dotenv.config();
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running on port 5000');
