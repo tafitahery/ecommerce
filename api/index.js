e89537fde8a4415e513fdeea0a8bd5a3e9952676
Tafita Hery <tafitahery@gmail.com>
2022-10-06 16:10:07 +0300

    Node.js CRUD Operation with JWT

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running on port 5000');
