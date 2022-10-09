a80e165479f661ff233314f65536689bdd0f055f
Tafita Hery <tafitahery@gmail.com>
2022-10-09 11:28:52 +0300

    How to Use React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,29 @@
-// import Home from './pages/Home';
-// import Product from './pages/Product';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Product from './pages/Product';
 import Cart from './pages/Cart';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import ProductList from './pages/ProductList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProductList from './pages/ProductList';
 
 const App = () => {
+  const user = true;
+
   return (
-    // <Home />
-    // <ProductList />
-    // <Product />
-    // <Register />
-    // <Login />
-    <Cart />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
